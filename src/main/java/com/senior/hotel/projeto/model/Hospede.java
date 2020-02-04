c8cd3dd6281a42248a742ef7607d011110f62da7
efsant0s <eduardo.felipe.santos@philips.com>
2020-02-04 20:11:03 -0300

    Commit inicial

---
+++ src/main/java/com/senior/hotel/projeto/model/Hospede.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.senior.hotel.projeto.model;
+/**
+ *
+ * @author Eduardo
+ */
+
+public class Hospede {
+    
+    private long id;
+    
+    private String nome;
+    
+    private String documento;
+}
---
+++ src/test/java/com/senior/hotel/ProjetoHotel/ProjetoHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senior.hotel.ProjetoHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
